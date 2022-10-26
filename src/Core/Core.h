04e5464cc73de0eab3168d4e0bef6070af0cc899
Posmac Nicolai <nicolai.posmac@gmail.com>
2022-10-26 10:01:23 +0300

    Added VK_CHECK_RESULT

--- src/Core/Core.h
+++ src/Core/Core.h
@@ -1,12 +1,23 @@
-#ifndef CORE
-#define CORE
+#pragma once
 
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cassert>
 #include "vulkan/vulkan.h"
 #include "vulkan/vk_platform.h"
 
-#define LOG_INFO(x) std::cout << (x) << std::endl
+namespace vk
+{
+    #define LOG_INFO(x) std::cout << (x) << std::endl
 
-#endif
+    #define VK_CHECK_RESULT(f) \
+    {																										\
+        VkResult res = (f);																					\
+        if (res != VK_SUCCESS) {																			\
+            std::cout << "Fatal : VkResult is \"" << (res) <<                                               \
+            "\" in " << __FILE__ << " at line " << __LINE__ << std::endl;                                   \
+            assert(res == VK_SUCCESS);																	    \
+        }																									\
+    }
+}
